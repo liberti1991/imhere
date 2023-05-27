80d9265b2bec9193b5bcef23e20f4b06749164a7
Rodrigo <liberti1991@gmail.com>
2023-05-27 11:00:06 -0300

    'native'

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.Text}>Hello My love!</Text>
+      <Text style={styles.Text}>I love you ❤️</Text>
+      <Text style={styles.Text}>by libeti</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Text: {
+    color: "#fff",
   },
 });
